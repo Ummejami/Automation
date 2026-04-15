90c15a4788d2dd652664149c6e804447de1d7cd4
Umme Jami <156930660+Ummejami@users.noreply.github.com>
2026-04-15 07:40:51 +0600

    Add ScreenshotUtil class for taking screenshots

--- Utils/Screenshotutil.java
+++ Utils/Screenshotutil.java
@@ -1,1 +1,12 @@
+package utils;
 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+public class ScreenshotUtil {
+
+    public static byte[] takeScreenshot(WebDriver driver) {
+        return ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+    }
+}
