b56335c552a95c3d1068c5d6e436ff9d422007e0
Umme Jami <156930660+Ummejami@users.noreply.github.com>
2026-04-15 07:45:27 +0600

    Update Homepage.java

--- src/main/java/pages/Homepage.java
+++ src/main/java/pages/Homepage.java
@@ -1,1 +1,42 @@
+package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class HomePage {
+
+    WebDriver driver;
+
+    public HomePage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    // Locators
+    private By title = By.className("title");
+    private By menuButton = By.id("react-burger-menu-btn");
+    private By logoutLink = By.id("logout_sidebar_link");
+    private By cartIcon = By.className("shopping_cart_link");
+
+    // Actions
+
+    public String getPageTitle() {
+        return driver.findElement(title).getText();
+    }
+
+    public boolean isCartIconDisplayed() {
+        return driver.findElement(cartIcon).isDisplayed();
+    }
+
+    public void clickMenu() {
+        driver.findElement(menuButton).click();
+    }
+
+    public void clickLogout() {
+        driver.findElement(logoutLink).click();
+    }
+
+    public void logout() {
+        clickMenu();
+        clickLogout();
+    }
+}
