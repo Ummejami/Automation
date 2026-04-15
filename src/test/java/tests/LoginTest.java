c2ede79c415d9d6f9c73356578e148e84b9be012
Umme Jami <156930660+Ummejami@users.noreply.github.com>
2026-04-15 07:41:52 +0600

    Add LoginTest class for login feature testing

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,1 +1,42 @@
+package tests;
 
+import io.qameta.allure.*;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+import pages.LoginPage;
+import utils.DriverManager;
+
+@Epic("Authentication")
+@Feature("Login Feature")
+public class LoginTest {
+
+    WebDriver driver;
+    LoginPage loginPage;
+
+    @BeforeMethod
+    public void setup() {
+        driver = DriverManager.getDriver();
+        driver.get("https://www.saucedemo.com/");
+        loginPage = new LoginPage(driver);
+    }
+
+    @Test(description = "Valid Login Test")
+    @Severity(SeverityLevel.CRITICAL)
+    @Description("Test login with valid credentials")
+    public void validLoginTest() {
+
+        loginPage.enterUsername("standard_user");
+        loginPage.enterPassword("secret_sauce");
+        loginPage.clickLogin();
+
+        String currentUrl = driver.getCurrentUrl();
+        Assert.assertTrue(currentUrl.contains("inventory"));
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        DriverManager.quitDriver();
+    }
+}
